9841a5e781dfefeafcee47a9c72ea7217236cd0c
Dipak Rahangdale <110284680+8408989979@users.noreply.github.com>
2024-12-14 12:15:34 +0530

    new changes

--- src/practise/PracClass1.java
+++ src/practise/PracClass1.java
@@ -4,6 +4,7 @@ public class PracClass1 {
 	public static void main(String[] args) {
 		System.out.println("Hello_class1,Branch A changes,Branch A changes,scnario2");
 		System.out.println("Branch B changes,changes B");
-		System.out.println("Branch B changes,changes B,scenario 2");
+		System.out.println("Branch B changes,changes B,scenario 2 and changes");
+		System.out.println("Branch B changes,changes B,scenario 2 and ");
 	}
 }
