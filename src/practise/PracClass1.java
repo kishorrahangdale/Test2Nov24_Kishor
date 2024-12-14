19e4f895043733416c772b61abfd825b2aadff10
Dipak Rahangdale <110284680+8408989979@users.noreply.github.com>
2024-12-14 11:17:26 +0530

    conflict resolve

--- src/practise/PracClass1.java
+++ src/practise/PracClass1.java
@@ -3,5 +3,6 @@ package practise;
 public class PracClass1 {
 	public static void main(String[] args) {
 		System.out.println("Hello_class1,Branch A changes");
+		System.out.println("Branch B changes");
 	}
 }
